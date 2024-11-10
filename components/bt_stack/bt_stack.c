146841a2582032c5ba4e49235d145ecc999c4091
Bartosz <bartoszpietrzak.dev@gmail.com>
2024-11-10 11:35:05 +0100

    populating with example

--- components/bt_stack/bt_stack.c
+++ components/bt_stack/bt_stack.c
@@ -1,5 +1,9 @@
 #include "bt_stack.h"
+#include "esp_log.h"
 #include "esp_spp_api.h"
+
+#define SPP_TAG "SPP"
+#define SPP_SERVER_NAME "SPP_SERVER"
 
 static const char local_device_name[] = LOCAL_DEVICE_NAME;
 static const esp_spp_mode_t esp_spp_mode = ESP_SPP_MODE_CB;
@@ -50,7 +54,6 @@ static void esp_spp_cb(esp_spp_cb_event_t event, esp_spp_cb_param_t *param) {
     ESP_LOGI(SPP_TAG, "ESP_SPP_CL_INIT_EVT");
     break;
   case ESP_SPP_DATA_IND_EVT:
-#if (SPP_SHOW_MODE == SPP_SHOW_DATA)
     /*
      * We only show the data in which the data length is less than 128 here. If
      * you want to print the data and the data rate is high, it is strongly
@@ -64,13 +67,6 @@ static void esp_spp_cb(esp_spp_cb_event_t event, esp_spp_cb_param_t *param) {
     if (param->data_ind.len < 128) {
       esp_log_buffer_hex("", param->data_ind.data, param->data_ind.len);
     }
-#else
-    gettimeofday(&time_new, NULL);
-    data_num += param->data_ind.len;
-    if (time_new.tv_sec - time_old.tv_sec >= 3) {
-      print_speed();
-    }
-#endif
     break;
   case ESP_SPP_CONG_EVT:
     ESP_LOGI(SPP_TAG, "ESP_SPP_CONG_EVT");
@@ -79,11 +75,7 @@ static void esp_spp_cb(esp_spp_cb_event_t event, esp_spp_cb_param_t *param) {
     ESP_LOGI(SPP_TAG, "ESP_SPP_WRITE_EVT");
     break;
   case ESP_SPP_SRV_OPEN_EVT:
-    ESP_LOGI(SPP_TAG,
-             "ESP_SPP_SRV_OPEN_EVT status:%d handle:%" PRIu32 ", rem_bda:[%s]",
-             param->srv_open.status, param->srv_open.handle,
-             bda2str(param->srv_open.rem_bda, bda_str, sizeof(bda_str)));
-    gettimeofday(&time_old, NULL);
+    ESP_LOGI(SPP_TAG, "ESP_SPP_SRV_OPEN_EVT status:%d", param->srv_open.status);
     break;
   case ESP_SPP_SRV_STOP_EVT:
     ESP_LOGI(SPP_TAG, "ESP_SPP_SRV_STOP_EVT");
@@ -94,4 +86,152 @@ static void esp_spp_cb(esp_spp_cb_event_t event, esp_spp_cb_param_t *param) {
   default:
     break;
   }
+}
+
+void esp_bt_gap_cb(esp_bt_gap_cb_event_t event, esp_bt_gap_cb_param_t *param) {
+  char bda_str[18] = {0};
+
+  switch (event) {
+  case ESP_BT_GAP_AUTH_CMPL_EVT: {
+    if (param->auth_cmpl.stat == ESP_BT_STATUS_SUCCESS) {
+      ESP_LOGI(SPP_TAG, "authentication success: %s bda:[%s]",
+               param->auth_cmpl.device_name,
+               bda2str(param->auth_cmpl.bda, bda_str, sizeof(bda_str)));
+    } else {
+      ESP_LOGE(SPP_TAG, "authentication failed, status:%d",
+               param->auth_cmpl.stat);
+    }
+    break;
+  }
+  case ESP_BT_GAP_PIN_REQ_EVT: {
+    ESP_LOGI(SPP_TAG, "ESP_BT_GAP_PIN_REQ_EVT min_16_digit:%d",
+             param->pin_req.min_16_digit);
+    if (param->pin_req.min_16_digit) {
+      ESP_LOGI(SPP_TAG, "Input pin code: 0000 0000 0000 0000");
+      esp_bt_pin_code_t pin_code = {0};
+      esp_bt_gap_pin_reply(param->pin_req.bda, true, 16, pin_code);
+    } else {
+      ESP_LOGI(SPP_TAG, "Input pin code: 1234");
+      esp_bt_pin_code_t pin_code;
+      pin_code[0] = '1';
+      pin_code[1] = '2';
+      pin_code[2] = '3';
+      pin_code[3] = '4';
+      esp_bt_gap_pin_reply(param->pin_req.bda, true, 4, pin_code);
+    }
+    break;
+  }
+
+#if (CONFIG_EXAMPLE_SSP_ENABLED == true)
+  case ESP_BT_GAP_CFM_REQ_EVT:
+    ESP_LOGI(
+        SPP_TAG,
+        "ESP_BT_GAP_CFM_REQ_EVT Please compare the numeric value: %" PRIu32,
+        param->cfm_req.num_val);
+    esp_bt_gap_ssp_confirm_reply(param->cfm_req.bda, true);
+    break;
+  case ESP_BT_GAP_KEY_NOTIF_EVT:
+    ESP_LOGI(SPP_TAG, "ESP_BT_GAP_KEY_NOTIF_EVT passkey:%" PRIu32,
+             param->key_notif.passkey);
+    break;
+  case ESP_BT_GAP_KEY_REQ_EVT:
+    ESP_LOGI(SPP_TAG, "ESP_BT_GAP_KEY_REQ_EVT Please enter passkey!");
+    break;
+#endif
+
+  case ESP_BT_GAP_MODE_CHG_EVT:
+    ESP_LOGI(SPP_TAG, "ESP_BT_GAP_MODE_CHG_EVT mode:%d bda:[%s]",
+             param->mode_chg.mode,
+             bda2str(param->mode_chg.bda, bda_str, sizeof(bda_str)));
+    break;
+
+  default: {
+    ESP_LOGI(SPP_TAG, "event: %d", event);
+    break;
+  }
+  }
+  return;
+}
+
+void app_main(void) {
+  char bda_str[18] = {0};
+  esp_err_t ret = nvs_flash_init();
+  if (ret == ESP_ERR_NVS_NO_FREE_PAGES ||
+      ret == ESP_ERR_NVS_NEW_VERSION_FOUND) {
+    ESP_ERROR_CHECK(nvs_flash_erase());
+    ret = nvs_flash_init();
+  }
+  ESP_ERROR_CHECK(ret);
+
+  ESP_ERROR_CHECK(esp_bt_controller_mem_release(ESP_BT_MODE_BLE));
+
+  esp_bt_controller_config_t bt_cfg = BT_CONTROLLER_INIT_CONFIG_DEFAULT();
+  if ((ret = esp_bt_controller_init(&bt_cfg)) != ESP_OK) {
+    ESP_LOGE(SPP_TAG, "%s initialize controller failed: %s", __func__,
+             esp_err_to_name(ret));
+    return;
+  }
+
+  if ((ret = esp_bt_controller_enable(ESP_BT_MODE_CLASSIC_BT)) != ESP_OK) {
+    ESP_LOGE(SPP_TAG, "%s enable controller failed: %s", __func__,
+             esp_err_to_name(ret));
+    return;
+  }
+
+  esp_bluedroid_config_t bluedroid_cfg = BT_BLUEDROID_INIT_CONFIG_DEFAULT();
+#if (CONFIG_EXAMPLE_SSP_ENABLED == false)
+  bluedroid_cfg.ssp_en = false;
+#endif
+  if ((ret = esp_bluedroid_init_with_cfg(&bluedroid_cfg)) != ESP_OK) {
+    ESP_LOGE(SPP_TAG, "%s initialize bluedroid failed: %s", __func__,
+             esp_err_to_name(ret));
+    return;
+  }
+
+  if ((ret = esp_bluedroid_enable()) != ESP_OK) {
+    ESP_LOGE(SPP_TAG, "%s enable bluedroid failed: %s", __func__,
+             esp_err_to_name(ret));
+    return;
+  }
+
+  if ((ret = esp_bt_gap_register_callback(esp_bt_gap_cb)) != ESP_OK) {
+    ESP_LOGE(SPP_TAG, "%s gap register failed: %s", __func__,
+             esp_err_to_name(ret));
+    return;
+  }
+
+  if ((ret = esp_spp_register_callback(esp_spp_cb)) != ESP_OK) {
+    ESP_LOGE(SPP_TAG, "%s spp register failed: %s", __func__,
+             esp_err_to_name(ret));
+    return;
+  }
+
+  esp_spp_cfg_t bt_spp_cfg = {
+      .mode = esp_spp_mode,
+      .enable_l2cap_ertm = esp_spp_enable_l2cap_ertm,
+      .tx_buffer_size = 0, /* Only used for ESP_SPP_MODE_VFS mode */
+  };
+  if ((ret = esp_spp_enhanced_init(&bt_spp_cfg)) != ESP_OK) {
+    ESP_LOGE(SPP_TAG, "%s spp init failed: %s", __func__, esp_err_to_name(ret));
+    return;
+  }
+
+#if (CONFIG_EXAMPLE_SSP_ENABLED == true)
+  /* Set default parameters for Secure Simple Pairing */
+  esp_bt_sp_param_t param_type = ESP_BT_SP_IOCAP_MODE;
+  esp_bt_io_cap_t iocap = ESP_BT_IO_CAP_IO;
+  esp_bt_gap_set_security_param(param_type, &iocap, sizeof(uint8_t));
+#endif
+
+  /*
+   * Set default parameters for Legacy Pairing
+   * Use variable pin, input pin code when pairing
+   */
+  esp_bt_pin_type_t pin_type = ESP_BT_PIN_TYPE_VARIABLE;
+  esp_bt_pin_code_t pin_code;
+  esp_bt_gap_set_pin(pin_type, 0, pin_code);
+
+  ESP_LOGI(
+      SPP_TAG, "Own address:[%s]",
+      bda2str((uint8_t *)esp_bt_dev_get_address(), bda_str, sizeof(bda_str)));
 }
